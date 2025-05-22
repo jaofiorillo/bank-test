b1adc3b188f25d015e59230ef9c4dd213724e41e
Fiorillo <joao.banci@xbrain.com.br>
2025-05-22 15:13:30 -0300

    first commit

---
+++ src/main/java/com/fiorillo/back_test/BackTestApplication.java
@@ -0,0 +1,13 @@
+package com.fiorillo.back_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTestApplication.class, args);
+	}
+
+}
